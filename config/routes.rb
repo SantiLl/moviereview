e5acc1cb0a7237d3292a55f991e7e51fa4801a59
Santiago Llapur <santillapur@gmail.com>
2020-01-14 08:19:44 -0300

    Movies controller CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/edit'
+  get 'movies/update'
+  get 'movies/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   devise_for :users
